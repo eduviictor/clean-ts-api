821e95469bcd31631b83c4e4347a3fc0d931dc90
eduviictor <eduvictornobrega@gmail.com>
2020-12-12 14:58:30 -0300

    perf: change rule of eslint

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,41 +1,41 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
-import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError, ok } from '../../helpers/http-helper'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols';
+import { MissingParamError, InvalidParamError } from '../../errors';
+import { badRequest, serverError, ok } from '../../helpers/http-helper';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
 
   constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
-    this.emailValidator = emailValidator
-    this.addAccount = addAccount
+    this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
+          return badRequest(new MissingParamError(field));
         }
       }
-      const { name, email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
       if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
+        return badRequest(new InvalidParamError('passwordConfirmation'));
       }
 
-      const isValid = this.emailValidator.isValid(email)
+      const isValid = this.emailValidator.isValid(email);
 
       if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
+        return badRequest(new InvalidParamError('email'));
       }
 
-      const account = await this.addAccount.add({ name, email, password })
-      return ok(account)
+      const account = await this.addAccount.add({ name, email, password });
+      return ok(account);
     } catch (error) {
-      console.log(error)
-      return serverError()
+      console.log(error);
+      return serverError();
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,23 +1,23 @@
-import { HttpResponse } from '../protocols'
-import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols';
+import { ServerError } from '../errors';
 
 export const badRequest = (error: Error): HttpResponse => (
   {
     statusCode: 400,
     body: error
   }
-)
+);
 
 export const serverError = (): HttpResponse => (
   {
     statusCode: 500,
     body: new ServerError()
   }
-)
+);
 
 export const ok = (data: any): HttpResponse => (
   {
     statusCode: 200,
     body: data
   }
-)
+);
